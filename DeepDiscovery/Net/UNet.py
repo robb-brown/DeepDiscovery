dd002c42095ca6d6e2b94fc1b3645d797d4df0f6
Robert A. Brown <robert.brown@mcgill.ca>
2018-01-15 21:23:03 -0500

    3D Unet added support for input noise

--- DeepDiscovery/Net/UNet.py
+++ DeepDiscovery/Net/UNet.py
@@ -267,7 +267,7 @@ class UNet3D(Net):
 				net = self.addLayer(tf.layers.Dropout(rate=self.inputDropoutProbability,name='InputDropout')).apply(net,training=True)
 			if self.inputNoise:
 				logger.info('Using input noise')
-				net = net + tf.random_normal(shape=tf.shape(net), mean=0.0, stddev=self.inputNoiseSigma, dtype='float')
+				net = tf.reshape(net + tf.random_normal(shape=tf.shape(net), mean=0.0, stddev=self.inputNoiseSigma, dtype='float'),tf.shape(net))
 
 			net = uNet(net,filterPlan = self.filterPlan,filterSize = self.filterSize,maxpool=self.maxpool,layerThickness=self.layerThickness,normalization=self.normalization,dimensions=3,skip=self.skipChannels)
 			for i in range(self.postUDepth):
