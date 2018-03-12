Fix to jaccard computation, improved layer tracking, etc.

DeepDiscovery/Net/Segmenter2D.py
@@ -22,9 +22,9 @@ class Segmenter2D(UNet2D):
 		with tf.variable_scope(self.name):
 			self.modelParameters['logits'] = None
 			init = tf.contrib.layers.xavier_initializer()
-			self.net = tf.layers.conv2d(self.net, filters=self.outputValues, kernel_size = 1, padding='same', activation=None,kernel_initializer = None,data_format='channels_last')
+			self.net = self.addLayer(tf.layers.conv2d(self.net, filters=self.outputValues, kernel_size = 1, padding='same', activation=None,kernel_initializer = None,data_format='channels_last'))
 			self.logits = self.net
-			self.y = self.output = self.net = tf.nn.softmax(self.logits,-1)
+			self.y = self.output = self.net = self.addLayer(tf.nn.softmax(self.logits,-1,name='softmax'))
 
 
 	def segment(self,image):
@@ -40,10 +40,9 @@ class Segmenter2D(UNet2D):
 
 	def preprocessInput(self,example,dimensionOrder=None):
 		dimensionOrder = example.get('dimensionOrder',None) if dimensionOrder is None else dimensionOrder
-		ret = dict(); ret.update(example); 
+		ret = dict(); ret.update(example);
 		ret['input'] = self.preprocessor.process(example['input'],dimensionOrder=dimensionOrder)
 		ret['truth'] = self.preprocessor.process(example['truth'],dimensionOrder=dimensionOrder,oneHot=True)
 		if 'attention' in example:
 			ret['attention'] = self.preprocessor.process(example['attention'],standardize=False)
 		return ret
-	

DeepDiscovery/Net/Segmenter3D.py
@@ -22,10 +22,10 @@ class Segmenter3D(UNet3D):
 		with tf.variable_scope(self.name):
 			self.modelParameters['logits'] = None
 			init = tf.contrib.layers.xavier_initializer()
-			self.net = tf.layers.conv3d(self.net, filters=self.outputValues, kernel_size = 1, padding='same', activation=None,kernel_initializer = None,data_format='channels_last')
+			self.net = self.addLayer(tf.layers.conv3d(self.net, filters=self.outputValues, kernel_size = 1, padding='same', activation=None,kernel_initializer = None,data_format='channels_last'))
 			self.logits = self.net
-			self.y = self.output = self.net = tf.nn.softmax(self.logits,-1)
-	
+			self.y = self.output = self.net = self.addLayer(tf.nn.softmax(self.logits,-1,name='softmax'))
+
 
 	def segment(self,image):
 		# output = lasagne.layers.get_output(self.net)

DeepDiscovery/Net/UNet.py
@@ -23,7 +23,7 @@ def upsample(x,factor,dataFormat='channels_last',name='repeat'):
 		for axis in range(startingAxis,startingAxis+len(x.shape)-2):
 			x = tf.keras.backend.repeat_elements(x,factor,axis=axis)
 	return x
-		
+
 
 
 def uNet(input,filterPlan,filterSize=(5,5),maxpool=False,layerThickness=1,dropout=None,normalization=None,activation=tf.nn.relu,dimensions=3,skip=1.0):
@@ -32,8 +32,8 @@ def uNet(input,filterPlan,filterSize=(5,5),maxpool=False,layerThickness=1,dropou
 		convDown = tf.layers.conv2d; convUp = tf.layers.conv2d_transpose; maxpoolF = tf.layers.max_pooling2d
 	else:
 		convDown = tf.layers.conv3d; convUp = tf.layers.conv3d_transpose; maxpoolF = tf.layers.max_pooling3d
-	
-	downLayers = [];
+
+	downLayers = []; layers = OrderedDict()
 	stride = 1 if maxpool else 2
 	layerThickness = 1 if layerThickness < 1 else layerThickness
 	net = input
@@ -58,18 +58,18 @@ def uNet(input,filterPlan,filterSize=(5,5),maxpool=False,layerThickness=1,dropou
 						activation = activation,
 						kernel_initializer = init,
 						data_format='channels_last',
-						name = 'UNet-DownConv{}'.format(level+1))
+						name = 'UNet-DownConv{}'.format(level+1)); layers[net.name] = net
 		if normalization is not None:
 			pass							# ROBB - add normalization sometime?
 		if not dropout is None:
-			net = tf.layers.dropout(net,rate=dropout,name='UNet-DownDropout{}'.format(level+1))
+			net = tf.layers.dropout(net,rate=dropout,name='UNet-DownDropout{}'.format(level+1)); layers[net.name] = net
 		if maxpool:
 			net = maxpoolF(	inputs=net,
 							strides = 1,
 							pool_size = 2,
 							padding='same',
 							data_format='channels_last',
-							name='UNet-DownMaxpool{}'.format(level+1),)
+							name='UNet-DownMaxpool{}'.format(level+1),); layers[net.name] = net
 		for extraIndex in range(layerThickness-1):
 			net = convDown(	inputs=net,
 							filters=nFilters,
@@ -78,20 +78,20 @@ def uNet(input,filterPlan,filterSize=(5,5),maxpool=False,layerThickness=1,dropou
 							data_format='channels_last',
 							activation = activation,
 							kernel_initializer = init,
-							name = 'UNet-DownExtraConv{}-{}'.format(level+1,extraIndex+1))
+							name = 'UNet-DownExtraConv{}-{}'.format(level+1,extraIndex+1)); layers[net.name] = net
 			if not normalization is None:
 				pass
 			if not dropout is None:
-				net = tf.layers.dropout(net,rate=dropout,name='UNet-DownDropout{}'.format(level+1))
-		downLayers.append(net)	
+				net = tf.layers.dropout(net,rate=dropout,name='UNet-DownDropout{}'.format(level+1)); layers[net.name] = net
+		downLayers.append(net)
 
 	bottom = net
-	
+
 	logger.debug('U Up')
 	for index in range(0,len(filterPlan)):
 		nFilters = filterPlan[-1-index]
 		# Upsample
-		net = upsample(net,stride)
+		net = upsample(net,stride); layers[net.name] = net
 		# Concatenation with skip layers
 		if not skip is None:
 			skipChannels = downLayers[::-1][index+1]
@@ -102,8 +102,8 @@ def uNet(input,filterPlan,filterSize=(5,5),maxpool=False,layerThickness=1,dropou
 				localSkip = skip
 			if localSkip > 0:
 				skipChannels = skipChannels[...,0:localSkip]
-				net = tf.concat([net,skipChannels],axis=-1,name='UNet-UpConcat{}'.format(index+1))
-		
+				net = tf.concat([net,skipChannels],axis=-1,name='UNet-UpConcat{}'.format(index+1)); layers[net.name] = net
+
 		net = convDown(	inputs=net,
 						filters=nFilters,
 						kernel_size=filterSize,
@@ -112,11 +112,11 @@ def uNet(input,filterPlan,filterSize=(5,5),maxpool=False,layerThickness=1,dropou
 						data_format='channels_last',
 						activation = activation,
 						kernel_initializer = init,
-						name = 'UNet-UpConv{}'.format(index+1))
+						name = 'UNet-UpConv{}'.format(index+1)); layers[net.name] = net
 		if not normalization is None:
 			pass
 		if not dropout is None:
-			net = tf.layers.dropout(net,rate=dropout,name='UNet-UpDropout{}'.format(index+1))
+			net = tf.layers.dropout(net,rate=dropout,name='UNet-UpDropout{}'.format(index+1)); layers[net.name] = net
 		if not index == len(filterPlan)-1:
 			for extraIndex in range(layerThickness-1):
 				net = convDown(	inputs=net,
@@ -126,13 +126,13 @@ def uNet(input,filterPlan,filterSize=(5,5),maxpool=False,layerThickness=1,dropou
 								data_format='channels_last',
 								activation = activation,
 								kernel_initializer = init,
-								name = 'UNet-UpExtraConv{}-{}'.format(index+1,extraIndex+1))
+								name = 'UNet-UpExtraConv{}-{}'.format(index+1,extraIndex+1)); layers[net.name] = net
 				if normalization is not None:
 					pass
 				if not dropout is None:
-					net = tf.layers.dropout(net,rate=dropout,name='UNet-UpDropout{}'.format(index+1))
-			
-	return net
+					net = tf.layers.dropout(net,rate=dropout,name='UNet-UpDropout{}'.format(index+1)); layers[net.name] = net
+
+	return net,layers
 
 
 
@@ -170,10 +170,10 @@ class UNet2D(Net):
 	def create(self):
 		"""Override this method to customize your model"""
 		init = tf.contrib.layers.xavier_initializer()
-		
+
 		with tf.variable_scope(self.name):
 			self.x = tf.placeholder('float',shape=self.dimensions,name='input')
-		
+
 			self.yp = tf.placeholder('float',shape=[None,None,None,None],name='truth')
 			self.modelParameters['attentionWeight'] = tf.placeholder('float',shape=[None,None,None,None],name='attention')
 
@@ -192,25 +192,26 @@ class UNet2D(Net):
 
 			net = inLayer = self.x
 			if self.inputDropout:
-				net = self.addLayer(tf.layers.Dropout(rate=self.inputDropoutProbability,name='InputDropout')).apply(net,training=True)
+				net = self.addLayer(tf.layers.Dropout(rate=self.inputDropoutProbability,name='InputDropout')).apply(net,training=True);
 			if self.inputNoise:
-				net = tf.reshape(net + tf.random_normal(shape=tf.shape(net), mean=0.0, stddev=self.inputNoiseSigma, dtype='float'),tf.shape(net))
+				net = self.addLayer(tf.reshape(net + tf.random_normal(shape=tf.shape(net), mean=0.0, stddev=self.inputNoiseSigma, dtype='float'),tf.shape(net),name='InputNoise'));
 
-			net = uNet(net,filterPlan = self.filterPlan,filterSize = self.filterSize,maxpool=self.maxpool,layerThickness=self.layerThickness,normalization=self.normalization,dimensions=2,skip=self.skipChannels)
+			net,ulayers = uNet(net,filterPlan = self.filterPlan,filterSize = self.filterSize,maxpool=self.maxpool,layerThickness=self.layerThickness,normalization=self.normalization,dimensions=2,skip=self.skipChannels);
+			self.layers.update(ulayers)
 			for i in range(self.postUDepth):
-				net = tf.layers.conv2d(	inputs=net,
+				net = self.addLayer(tf.layers.conv2d(	inputs=net,
 										filters=self.filterPlan[0],
 										kernel_size=self.filterSize,
 										padding='same',
 										activation = self.nonlinearity,
 										kernel_initializer = init,
 										data_format='channels_last',
-										name = 'PostU{}'.format(i+1))
-			
+										name = 'PostU{}'.format(i+1)));
+
 				if self.normalization is not None:
 					pass
 				if self.internalDropout:
-					net = tf.layers.dropout(net,rate=self.internalDropoutProbability,name='PostUDropout{}'.format(i+1))
+					net = self.addLayer(tf.layers.dropout(net,rate=self.internalDropoutProbability,name='PostUDropout{}'.format(i+1)))
 			self.requiredInputs = [self.x]
 			if self.inputDropoutProbability is not None:
 				self.requiredInputs += [self.inputDropoutProbability]
@@ -224,7 +225,7 @@ class UNet2D(Net):
 
 	def preprocessInput(self,example,dimensionOrder=None):
 		dimensionOrder = example.get(dimensionOrder,None) if dimensionOrder is None else dimensionOrder
-		ret = dict(); ret.update(example); 
+		ret = dict(); ret.update(example);
 		ret['input'] = self.preprocessor.process(example['input'],dimensionOrder=dimensionOrder)
 		ret['truth'] = self.preprocessor.process(example['truth'],dimensionOrder=dimensionOrder)
 		return ret
@@ -289,26 +290,27 @@ class UNet3D(Net):
 
 			net = inLayer = self.x
 			if self.inputDropout:
-				net = self.addLayer(tf.layers.Dropout(rate=self.inputDropoutProbability,name='InputDropout')).apply(net,training=True)
+				net = self.addLayer(tf.layers.Dropout(rate=self.inputDropoutProbability,name='InputDropout')).apply(net,training=True);
 			if self.inputNoise:
 				#logger.info('Using input noise')
-				net = tf.reshape(net + tf.random_normal(shape=tf.shape(net), mean=0.0, stddev=self.inputNoiseSigma, dtype='float'),tf.shape(net))
+				net = self.addLayer(tf.reshape(net + tf.random_normal(shape=tf.shape(net), mean=0.0, stddev=self.inputNoiseSigma, dtype='float'),tf.shape(net),name='inputNoise'))
 
-			net = uNet(net,filterPlan = self.filterPlan,filterSize = self.filterSize,maxpool=self.maxpool,layerThickness=self.layerThickness,normalization=self.normalization,dimensions=3,skip=self.skipChannels)
+			net,ulayers = uNet(net,filterPlan = self.filterPlan,filterSize = self.filterSize,maxpool=self.maxpool,layerThickness=self.layerThickness,normalization=self.normalization,dimensions=3,skip=self.skipChannels)
+			self.layers.update(ulayers)
 			for i in range(self.postUDepth):
-				net = tf.layers.conv3d(	inputs=net,
+				net = self.addLayer(tf.layers.conv3d(	inputs=net,
 										filters=self.filterPlan[0],
 										kernel_size=self.filterSize,
 										padding='same',
 										activation = self.nonlinearity,
 										kernel_initializer = init,
 										data_format='channels_last',
-										name = 'PostU{}'.format(i+1))
+										name = 'PostU{}'.format(i+1)))
 
 				if self.normalization is not None:
 					pass
 				if self.internalDropout:
-					net = tf.layers.dropout(net,rate=self.internalDropoutProbability,name='PostUDropout{}'.format(i+1))
+					net = self.addLayer(tf.layers.dropout(net,rate=self.internalDropoutProbability,name='PostUDropout{}'.format(i+1)))
 			self.requiredInputs = [self.x]
 			if self.inputDropoutProbability is not None:
 				self.requiredInputs += [self.inputDropoutProbability]
@@ -322,13 +324,7 @@ class UNet3D(Net):
 
 	def preprocessInput(self,example,dimensionOrder=None):
 		dimensionOrder = example.get(dimensionOrder,None) if dimensionOrder is None else dimensionOrder
-		ret = dict(); ret.update(example); 
+		ret = dict(); ret.update(example);
 		ret['input'] = self.preprocessor.process(example['input'],dimensionOrder=dimensionOrder)
 		ret['truth'] = self.preprocessor.process(example['truth'],dimensionOrder=dimensionOrder)
 		return ret
-
-
-
-
-
-

DeepDiscovery/Trainer/Trainer.py
@@ -6,12 +6,12 @@ import logging
 logger = logging.getLogger(__name__)
 
 class CostFunction(DeepRoot.DeepRoot):
-	
+
 	def __new__(cls,*args,**kwargs):
 		self = super().__new__(cls)
 		self.__dict__['modelParameters'].update(dict(requiredInputs=[],net=None))
 		return self
-	
+
 	def create(self):
 		raise NotImplementedError
 
@@ -23,11 +23,11 @@ class CrossEntropyCost(CostFunction):
 		self.__dict__['modelParameters'].update(dict(y = None, yp = None))
 		self.__dict__['attention'] = None
 		return self
-	
+
 	def __init__(self,net,attention=False):
 		self.net = net
 		self.attention = attention
-	
+
 	def create(self):
 		cost = tf.keras.losses.categorical_crossentropy(self.net.yp,self.net.y)
 		if self.attention:
@@ -41,7 +41,7 @@ class CrossEntropyCost(CostFunction):
 
 
 class Trainer(DeepRoot.DeepRoot):
-	
+
 	def __new__(cls,*args,**kwargs):
 		self = super().__new__(cls)
 		self.__dict__['modelParameters'].update(dict(session = None,
@@ -50,7 +50,7 @@ class Trainer(DeepRoot.DeepRoot):
 													updates = None,
 													metrics=None))
 		return self
-	
+
 
 	def __init__(self,net,cost,examples,progressTracker=None,metrics=dict(cost=None),name=None,**trainingArguments):
 		self.__dict__['examples'] = examples
@@ -103,14 +103,13 @@ class Trainer(DeepRoot.DeepRoot):
 						accuracy = tf.reduce_mean(tf.cast(correctPrediction,'float'))
 						metrics[metric] = accuracy
 					elif metric == 'jaccard':
-						output = tf.cast(tf.argmax(net.y,axis=-1), dtype=tf.float32)
-						truth = tf.cast(tf.argmax(net.yp,axis=-1), dtype=tf.float32)
-						intersection = tf.reduce_sum(tf.reduce_sum(tf.multiply(output, truth), axis=-1),axis=-1)
-						union = tf.reduce_sum(tf.reduce_sum(tf.cast(tf.add(output, truth)>= 1, dtype=tf.float32), axis=-1),axis=-1) + 0.0000001
+						output = tf.cast(net.y[...,1]>0.5, dtype=tf.float32)
+						truth = tf.cast(net.yp[...,1]>0.5, dtype=tf.float32)
+						intersection = tf.reduce_sum(tf.multiply(output, truth))
+						union = tf.reduce_sum(tf.cast(tf.add(output, truth) >= 1,dtype=tf.float32))
 						jaccard = tf.reduce_mean(intersection / union)
 						metrics[metric] = jaccard
 			self.metrics = metrics
-		
 
 
 	def trainOne(self,examples=None,N=1,**args):
@@ -164,7 +163,7 @@ class Trainer(DeepRoot.DeepRoot):
 			# -------------------  Validation --------------------
 			example = self.examples.getValidationExamples(validationExamplesPerBatch)
 			result = self.validate(example,**validateArgs)
-			
+
 			if self.progressTracker is not None:
 				self.progressTracker.update(example=example,epoch=self.epoch,iteration=iteration,elapsed=self.previouslyElapsed+self.elapsed,totalIterations=self.epoch*examplesPerEpoch+iteration,kind='Validation',metrics=result)
 			self.epoch += 1; t2 = time.time(); elapsed = t2-self.startTime;
@@ -232,20 +231,20 @@ class Trainer(DeepRoot.DeepRoot):
 		self.__dict__['saveTime'] = time.time()
 
 		os.makedirs(os.path.join(path,trainerName),mode=0o777,exist_ok=True)
-		
+
 		# save the net and tracker and replace their references in the trainer with file names
 		self.netClass = self.net.__class__
 		self.trackerClass = self.progressTracker.__class__
 		self.netFile = os.path.basename(self.net.save(path))
 		self.trackerFile = os.path.basename(self.progressTracker.save(path))
-		
+
 		with open(os.path.join(path,trainerName,trainerName),'wb') as f:
 			dill.dump(self,f,protocol=dill.HIGHEST_PROTOCOL)
 
 		variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=self.name)
 		self.modelParameters['tfSaver'] = tf.train.Saver(var_list=variables)
 		self.saveCheckpoint(write_meta_graph=True)
-		
+
 		return os.path.join(path,trainerName,trainerName)
 
 
@@ -282,4 +281,3 @@ class Trainer(DeepRoot.DeepRoot):
 			logger.exception('Failed to load net')
 			logger.warning('*** Could not locate saved network at {}. Trainer will be nonfunctional. ***'.format(fname))
 		return trainer
-