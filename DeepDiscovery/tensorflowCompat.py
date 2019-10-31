cb3979c8ac35baba35d903bb9669eb7f868baf64
Robert Brown <robert.brown@mcgill.ca>
2019-10-31 11:57:53 -0400

    Bugfix in tensorflow compatability layer

--- DeepDiscovery/tensorflowCompat.py
+++ DeepDiscovery/tensorflowCompat.py
@@ -1,7 +1,8 @@
 try:		# Tensorflow 2 stuff
-	import tensorflow as tf2
+	import tensorflow.compat.v2 as tf2
 	import tensorflow.compat.v1 as tf
 	tf.disable_eager_execution()
-	tf.set_random_seed = lambda seed: tf2.random.set_seed(seed)
+	if not hasattr(tf,'set_random_seed'): 
+		tf.set_random_seed = lambda seed: tf2.random.set_seed(seed)
 except:
 	import tensorflow as tf
